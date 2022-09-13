ae151995cad3cf1e1c02760c661b56aea531c3d5
Bass2015 <bass.dolg@gmail.com>
2022-09-13 20:14:03 +0200

    set basic animation loop

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from datetime import datetime
-from js import document, Math, setInterval
+from js import document, requestAnimationFrame
 from pyodide import create_proxy
 import math
 
 canvas = document.getElementById("canvas")
 ctx = canvas.getContext("2d")
 keysdown = []
+circle = {
+    'x': canvas.width/2,
+    'y': canvas.height/2
+}
 
 def on_key_down(*args):
     if args[0].key not in keysdown:
@@ -17,23 +21,40 @@ def on_key_up(*args):
     keysdown.remove(args[0].key)
 
 def render(*args):
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.beginPath()
-    ctx.arc(canvas.width/2, canvas.height/2, 10, 0, math.pi*2)
+    ctx.arc(circle['x'], circle['y'], 10, 0, math.pi*2)
     ctx.fillStyle = 'red'
     if 'b' in keysdown:
         ctx.fillStyle = 'blue'
     if 'g' in keysdown: 
         ctx.fillStyle = 'green'
     ctx.fill()
-   
+
+def update():
+    if 'd' in keysdown:
+        circle['x'] += 1
+    if 'a' in keysdown:
+        circle['x'] -= 1
+    if 'w' in keysdown:
+        circle['y'] -= 1
+    if 's' in keysdown:
+        circle['y'] += 1
+
+def game_loop(*args):
+    update()
+    render()
+    requestAnimationFrame(game_loop_proxy)
+    
+    
+game_loop_proxy = create_proxy(game_loop)
+
 def main():
     kdown_proxy = create_proxy(on_key_down)
     kup_proxy = create_proxy(on_key_up)
-    render_proxy = create_proxy(render)
     document.addEventListener("keydown", kdown_proxy)
     document.addEventListener("keyup", kup_proxy)
-    
-
+    requestAnimationFrame(game_loop_proxy)
 
 main()
  
