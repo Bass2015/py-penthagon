c8677fb1acebff7b32ea7ed97a67ea75fa16bd72
Bass2015 <bass.dolg@gmail.com>
2022-09-22 14:17:04 +0200

    Hearts respond to player hit

--- src/ui_manager.py
+++ src/ui_manager.py
@@ -1,8 +1,36 @@
 from js import document
+import constants
 
 class UIManager():
     def __init__(self):
+        constants.SHIP_EXPLODED.suscribe(self)
         self.play_b = document.getElementById("play_b")
         self.play_b.style.display = 'none'
-        pass
+        self.players_ui = {
+            1: {
+                'score': document.getElementById("score_p1"),
+                'hearts': [
+                    document.getElementById("heart1_0"),
+                    document.getElementById("heart1_1"),
+                    document.getElementById("heart1_2")
+                ],
+                'lifes': 3
+            },
+            2: {
+                'score': document.getElementById("score_p2"),
+                'hearts': [
+                    document.getElementById("heart2_0"),
+                    document.getElementById("heart2_1"),
+                    document.getElementById("heart2_2")
+                ],
+                'lifes': 3
+            }
+        }
+    
+    def on_ship_exploded(self, ship):
+        lifes = self.players_ui[ship.player]['lifes']
+        self.players_ui[ship.player]['hearts'][lifes - 1].style.display = 'none'
+        if lifes > 1:
+            self.players_ui[ship.player]['lifes'] -= 1
+
 
