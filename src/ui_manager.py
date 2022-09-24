a0f36e44336b4ffe6230008cc92f6ea59c3c70a7
Bass2015 <bass.dolg@gmail.com>
2022-09-24 18:35:18 +0200

    added  UI layer

--- src/ui_manager.py
+++ src/ui_manager.py
@@ -3,29 +3,30 @@ import constants
 
 class UIManager():
     def __init__(self):
-        constants.SHIP_EXPLODED.suscribe(self)
-        self.play_b = document.getElementById("play_b")
-        self.play_b.style.display = 'none'
-        self.players_ui = {
-            1: {
-                'score': document.getElementById("score_p1"),
-                'hearts': [
-                    document.getElementById("heart1_0"),
-                    document.getElementById("heart1_1"),
-                    document.getElementById("heart1_2")
-                ],
-                'lifes': 3
-            },
-            2: {
-                'score': document.getElementById("score_p2"),
-                'hearts': [
-                    document.getElementById("heart2_0"),
-                    document.getElementById("heart2_1"),
-                    document.getElementById("heart2_2")
-                ],
-                'lifes': 3
-            }
-        }
+        # constants.SHIP_EXPLODED.suscribe(self)
+        # self.play_b = document.getElementById("play_b")
+        # self.play_b.style.display = 'none'
+        # self.players_ui = {
+        #     1: {
+        #         'score': document.getElementById("score_p1"),
+        #         'hearts': [
+        #             document.getElementById("heart1_0"),
+        #             document.getElementById("heart1_1"),
+        #             document.getElementById("heart1_2")
+        #         ],
+        #         'lifes': 3
+        #     },
+        #     2: {
+        #         'score': document.getElementById("score_p2"),
+        #         'hearts': [
+        #             document.getElementById("heart2_0"),
+        #             document.getElementById("heart2_1"),
+        #             document.getElementById("heart2_2")
+        #         ],
+        #         'lifes': 3
+        #     }
+        # }
+        pass
     
     def on_ship_exploded(self, ship):
         lifes = self.players_ui[ship.player]['lifes']
