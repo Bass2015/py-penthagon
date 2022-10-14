ca05d826d9ae50063c7b714e05a1a433938f5251
Bass2015 <bass.dolg@gmail.com>
2022-10-14 19:50:00 +0200

    fixed physics.check_objects

--- src/physics.py
+++ src/physics.py
@@ -3,13 +3,17 @@ from objects import GameObject, Bullet, Ship
 import constants
 import events
 def check_objects(asteroids, bullets, ships):
-    for asteroid in asteroids:
-        for ship in ships:
-            check_collision(asteroid, ship)
+    for ship in ships:
+        if asteroids:
+            for asteroid in asteroids:
+                check_collision(asteroid, ship)
+                for bullet in bullets:
+                    check_collision(ship, bullet)
+                    check_collision(asteroid, bullet)
+        elif bullets:
             for bullet in bullets:
                 check_collision(ship, bullet)
-                check_collision(asteroid, bullet)
-   
+            
 def check_collision(obj1:GameObject, obj2:GameObject):
     if (not obj1.active or not obj2.active):
         return
