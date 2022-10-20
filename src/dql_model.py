c84dc86a3f45ed6f8b3b0c79b2cd82bb39b34f1c
Bass2015 <bass.dolg@gmail.com>
2022-10-20 18:17:01 +0200

    started building network

--- src/dql_model.py
+++ src/dql_model.py
@@ -1,5 +1,5 @@
 import random
-from cnn.src.model import Network
+from cnnB.model.layers import Conv2D, FullyConnected as FC
 
 class Brain():
     def __init__(self) -> None:
@@ -8,3 +8,19 @@ class Brain():
     def act(self, state):
         return self.network(state)
     
+class Network:
+    def __init__(self):
+        self.conv = []
+        self.fully_connected = []
+        self.build_network()
+
+    def build_network(self):
+        self.conv.append((Conv2D(4, 32, kernel_size=7, stride=4), 
+                            Conv2D(32, 64, kernel_size=5, stride=2),
+                            Conv2D(4, 32, kernel_size=7, stride=4)))
+
+    def forward(self):
+        # hacer forward por las convolutional layers
+        # flatten el output
+        # hacer forward con las fully connected
+        pass
